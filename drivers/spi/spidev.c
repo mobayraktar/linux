46cba8b9ef0477d4e8b99e7e21390a15a8d8a89c
Muhammet Onur Bayraktar <mobayraktar@t3gemstone.org>
2026-05-04 11:21:26 +0300

    add t3-gem-o1

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -711,6 +711,8 @@ static const struct spi_device_id spidev_spi_ids[] = {
 	{ .name = "spi-authenta" },
 	{ .name = "em3581" },
 	{ .name = "si3210" },
+	{ .name = "bmp390-spidev" },
+	{ .name = "icm20948-spidev" },
 	{},
 };
 MODULE_DEVICE_TABLE(spi, spidev_spi_ids);
@@ -741,6 +743,8 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "semtech,sx1301", .data = &spidev_of_check },
 	{ .compatible = "silabs,em3581", .data = &spidev_of_check },
 	{ .compatible = "silabs,si3210", .data = &spidev_of_check },
+	{ .compatible = "bosch,bmp390-spidev", .data = &spidev_of_check },
+	{ .compatible = "invensense,icm20948-spidev", .data = &spidev_of_check },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
